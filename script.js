47ab6d976b0b951cd21067ba33731763d3aefe29
Tafari <Tafaric@outlook.com>
2024-08-09 12:29:18 +0100

    Product Page
    Invntz Product section

---
+++ script.js
@@ -0,0 +1,21 @@
+let typed = new Typed(".auto-type", {
+  strings: [
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              voluptas quia pariatur dolorum voluptate obcaecati ipsam rerum
+              eius debitis. Expedita architecto est reprehenderit dolorem optio
+              quibusdam ab animi provident eaque! Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Fuga voluptas quia pariatur dolorum
+              voluptate obcaecati ipsam rerum eius debitis. Expedita architecto
+              est reprehenderit dolorem optio quibusdam ab animi provident
+              eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              `,
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  loop: true,
+});
+
+let typed2 = new Typed(".auto-type2", {
+  strings: ["Product Name"],
+  typeSpeed: 100,
+});
